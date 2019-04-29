b1e0f221ebdc8b5f9eb51e3b36e7fdecf686b308
Evgeniy Nochkin <nochjo@yandex.ru>
2019-04-29 14:49:04 +0300

    29.04.2019(15)

--- FirstTask/src/test/DBWorkTest.java
+++ FirstTask/src/test/DBWorkTest.java
@@ -1,9 +1,6 @@
 import dataset.UsersDataSet;
 import exception.DBException;
-import org.hibernate.SessionFactory;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.service.ServiceRegistry;
+import org.junit.Before;
 import org.junit.Test;
 import service.DBService;
 
@@ -11,34 +8,12 @@ import static org.junit.Assert.assertTrue;
 
 public class DBWorkTest {
 
-    private static final String hibernate_show_sql = "true";
-    private static final String hibernate_hbm2ddl_auto = "create";
+    DBService dbService;
 
-    Configuration configuration = getH2Configuration();
-    private final SessionFactory sessionFactory = createSessionFactory(configuration);
-
-    private Configuration getH2Configuration() {
-        Configuration configuration = new Configuration();
-        configuration.addAnnotatedClass(UsersDataSet.class);
-
-        configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
-        configuration.setProperty("hibernate.connection.driver_class", "org.h2.Driver");
-        configuration.setProperty("hibernate.connection.url", "jdbc:h2:./h2db");
-        configuration.setProperty("hibernate.connection.username", "tully");
-        configuration.setProperty("hibernate.connection.password", "tully");
-        configuration.setProperty("hibernate.show_sql", hibernate_show_sql);
-        configuration.setProperty("hibernate.hbm2ddl.auto", hibernate_hbm2ddl_auto);
-        return configuration;
+    @Before
+    public void init() {
+        dbService = new DBService();
     }
-
-    private static SessionFactory createSessionFactory(Configuration configuration) {
-        StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder();
-        builder.applySettings(configuration.getProperties());
-        ServiceRegistry serviceRegistry = builder.build();
-        return configuration.buildSessionFactory(serviceRegistry);
-    }
-
-    DBService dbService = new DBService();
 
     //создание пользователя без имени
     @Test
