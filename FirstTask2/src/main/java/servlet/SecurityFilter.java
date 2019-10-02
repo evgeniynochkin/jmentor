961626d4a5b89035605abaecb85622a2b58c9342
Evgeniy Nochkin <nochjo@yandex.ru>
2019-10-02 21:29:17 +0300

    NewTask2 02.10.19 (3)

--- FirstTask2/src/main/java/servlet/SecurityFilter.java
+++ FirstTask2/src/main/java/servlet/SecurityFilter.java
@@ -6,72 +6,73 @@ import service.AppUtils;
 import service.UserRoleRequestWrapper;
 
 import javax.servlet.*;
-import javax.servlet.annotation.WebServlet;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.sql.SQLException;
 
-@WebServlet("/*")
+@WebFilter(urlPatterns = {"/*"})
 public class SecurityFilter implements Filter {
 
-    private UserDataSet uds;
-
-    public SecurityFilter() { }
+    public SecurityFilter() {
+    }
 
     @Override
-    public void init(FilterConfig filterConfig) throws ServletException { }
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
             throws IOException, ServletException {
 
+        UserDataSet uds;
+
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpServletResponse response = (HttpServletResponse) servletResponse;
-//
-//        String servletPath = request.getServletPath();
-//
-//        uds = AppUtils.getLoginedUser(request.getSession());
 
-//        if (servletPath.equals("/login")) {
-//            filterChain.doFilter(request, response);
-//            return;
-//        }
-//
-//        HttpServletRequest wrapRequest = request;
-//
-//        if (uds != null) {
-//
-//            String userName = uds.getName();
-//            String userRole = uds.getRole();
-//
-//            wrapRequest = new UserRoleRequestWrapper(userName, userRole, request);
-//        }
-//
-//        if (SecurityUtils.isSecurityPage(request)) {
-//
-//            if (uds == null) {
-//
-//                String requestUri = request.getRequestURI();
-//
-//                int redirectId = AppUtils.storeRedirectAfterLoginUrl(request.getSession(), requestUri);
-//
-//                response.sendRedirect(wrapRequest.getContextPath() + "/login?redirectId=" + redirectId);
-//                return;
-//            }
-//
-//            boolean hasPermission = SecurityUtils.hasPermission(wrapRequest);
-//            if (!hasPermission) {
-//                RequestDispatcher dispatcher = request.getServletContext().getRequestDispatcher("/webapp/accessDeniedView.jsp");
-//
-//                dispatcher.forward(request, response);
-//                return;
-//            }
-//        }
+        String servletPath = request.getServletPath();
+
+        //Получаем пользователя сессии
+        uds = AppUtils.getLoginedUser(request.getSession());
+
+        //Если страница входа
+        if (servletPath.equals("/login")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        HttpServletRequest wrapRequest = request;
+
+        //Если пользователь уже вошел
+        if (uds != null) {
+
+            String userName = uds.getName();
+            String userRole = uds.getRole();
+
+            wrapRequest = new UserRoleRequestWrapper(userName, userRole, request);
+        }
+
+        //
+        if (SecurityUtils.isSecurityPage(request)) {
+
+            if (uds == null) {
+                response.sendRedirect(wrapRequest.getContextPath() + "/login");
+                return;
+            }
+
+            boolean hasPermission = SecurityUtils.hasPermission(wrapRequest);
+            if (!hasPermission) {
+                RequestDispatcher dispatcher = request.getServletContext().getRequestDispatcher("/webapp/accessDeniedView.jsp");
+
+                dispatcher.forward(request, response);
+                return;
+            }
+        }
 
         filterChain.doFilter(request, response);
     }
 
     @Override
-    public void destroy() { }
+    public void destroy() {
+    }
 }
---
+++ FirstTask2/src/main/webapp/accessDenied.jsp
@@ -0,0 +1,23 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: nochj
+  Date: 02.10.2019
+  Time: 21:23
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Access Denied</title>
+</head>
+<body>
+
+<jsp:include page="Menu.jsp"></jsp:include>
+
+<br/><br/>
+
+<h3 style="color:red;">Access Denied!</h3>
+
+</body>
+</html>
