8dfed436473db72e1ab6d3d5228c7f30e8e3ce3e
Evgeniy Nochkin <nochjo@yandex.ru>
2019-11-05 21:41:20 +0300

    05/11/19

---
+++ springtask/src/main/java/config/WebMvcConfig.java
@@ -0,0 +1,21 @@
+package config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("controllers")
+public class WebMvcConfig {
+
+    @Bean
+    public InternalResourceViewResolver getViewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+}
---
+++ springtask/web/WEB-INF/welcome.jsp
@@ -0,0 +1,8 @@
+<html>
+<body>
+
+<h2>Hello ${name}</h2>
+<p>Welcome to Spring MVC world.</p>
+
+</body>
+</html>
